208e25bcf6f04509ce8e26e2bbbd8491a7b967e4
ardigermenji <ardi.germenji@gmail.com>
2021-02-23 08:47:00 +0100

    refactor

--- app.js
+++ app.js
@@ -23,16 +23,3 @@ const dice = () => Math.random() * 1000;
 document
   .querySelectorAll('.row .col img')
   .forEach((img) => (img.src = `${img.src.slice(0, -1)}${dice()}`));
-
-const chevrons = document.querySelectorAll('i.fas');
-chevrons.forEach((chevron) => chevron.addEventListener('click', scrollRow));
-function scrollRow(e) {
-  if (e.target.classList.contains('fa-chevron-circle-left')) {
-    row.scrollLeft -= 1000;
-
-    console.log('left clicked');
-  } else {
-    console.log('right clicked');
-    row.scrollLeft += 1000;
-  }
-}
